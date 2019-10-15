c8c6f618994d722d8381c47d2b271cb812d8eb16
Yeji15Hwang <51129843+Yeji15Hwang@users.noreply.github.com>
2019-10-15 20:21:05 +0700

    script coin
    script coin

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    GameObject CanvasInfo;
+
+    void Awake()
+    {
+        CanvasInfo = GameObject.Find("CanvasInfo");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Destroy(gameObject);
+            CanvasInfo.GetComponent<CanvasInfo>().tambahKoin();
+        }
+    }
+}
