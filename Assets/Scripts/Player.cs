0180080f6d3612002f3a9c4ac2460de89dbc8e5c
Arnold1401 <Arnold.pramudita.wewe@gmail.com>
2019-10-15 20:04:27 +0700

    tsmbahn script character

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    bool balik; 
+
+    Rigidbody2D rb; 
+
+    SpriteRenderer sp; 
+
+    [SerializeField] 
+    Transform CekGround; 
+
+    [SerializeField] 
+    int kecepatan; 
+
+    [SerializeField] 
+    int jarak; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         rb = GetComponent<Rigidbody2D>(); 
+        sp = GetComponent<SpriteRenderer>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w")) 
+        { 
+
+            rb.velocity = new Vector2(0, kecepatan); 
+
+            sp.flipY = false; 
+
+        }else if (Input.GetKey("a")) 
+        { 
+
+            rb.velocity = new Vector2(-kecepatan, 0); 
+
+            sp.flipX = false; 
+
+        }else if (Input.GetKey("s"))
+        { 
+
+            rb.velocity = new Vector2(0, -kecepatan); 
+
+            sp.flipY = false; 
+
+        } 
+
+        else if (Input.GetKey("d")) 
+
+        { 
+
+            rb.velocity = new Vector2(kecepatan, 0); 
+
+            sp.flipX = false; 
+
+        } 
+    }
+
+}
