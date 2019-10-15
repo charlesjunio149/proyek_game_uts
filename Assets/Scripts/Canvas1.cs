08afb0f1adbd840d1266129caeb1eea0fa6bab07
Arnold1401 <Arnold.pramudita.wewe@gmail.com>
2019-10-15 12:29:12 +0700

    codingan time
    buat codingan time sama insert scriot di vanvas

---
+++ Assets/Scripts/Canvas1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+ 
+
+public class Canvas1 : MonoBehaviour
+{
+    [SerializeField] 
+
+    Text txttime; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
